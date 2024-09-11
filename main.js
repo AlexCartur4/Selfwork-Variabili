d29729a74f633e716a2981fa25bf8813d468a3b5
Alex Cartura <demonrose66@gmail.com>
2024-09-11 19:24:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let num1 = 5;
+
+let num2 = 10;
+
+console.log('Il valore della variabile num1 è ' + num1);
+
+console.log('Il valore della variabile num2 è ' + num2);
+
+let stringa = 'riccardo';
+
+console.log('Il valore della stringa è ' + stringa);
+
+const PIGRECO = 3.14159265359;
+
+console.log("il valore di PIGRECO e' " + PIGRECO);
+
+PIGRECO = 10  
